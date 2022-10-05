6db5ed112972f149c6cb021b943d3bab604f71d6
amourtialon <allan.mourtialon@hotmail.fr>
2022-10-05 16:45:54 +0200

    Update Send-Card.ps1

--- scripts/Send-Card.ps1
+++ scripts/Send-Card.ps1
@@ -1,1 +1,37 @@
+param(
+  [Parameter(Mandatory=$true)]
+  $From,
+  [Parameter(Mandatory=$true)]
+  $To,
+  [Parameter(Mandatory=$true)]
+  $FullPathCard
+)
 
+Connect-MgGraph -Scopes "Mail.Send"
+
+$MailBody = (Get-Content .\MailBody.html)
+$Card = (Get-Content $FullPathCard -Encoding utf8) | ConvertFrom-Json
+
+$CardJson = $Card | ConvertTo-Json -Depth 7
+
+$Message = $MailBody.Replace("%CARD%",$CardJson)
+
+$Body = @{
+
+    message = @{
+        subject = "$Subject"
+    
+    body = @{
+        contentType = "HTML"
+        content     = "$Message"
+    }
+    toRecipients =  @(@{
+        emailAddress = @{
+            address = "$To"
+        }}
+    
+    )
+    }
+}
+
+Send-MgUserMail -id $To -BodyParameter $Body
